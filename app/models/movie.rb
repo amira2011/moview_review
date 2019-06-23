9eb1842be2a563afcf189c51609becf21ce44e3c
abid <shaikh.abidali@gmail.com>
2019-06-23 13:54:37 +0530

    Validation

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,5 +2,7 @@ class Movie < ApplicationRecord
   belongs_to :user
   has_many :reviews
   has_one_attached :image
- scope :search, lambda {|query| where(["title LIKE ?","%#{query}%"])}
+  scope :search, lambda {|query| where(["title LIKE ?","%#{query}%"])}
+
+  validates :title, :description, :movie_length, :director, :rating, :image, presence: true
 end
