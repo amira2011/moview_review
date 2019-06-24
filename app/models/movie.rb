febb7425156ed4cfe84b3c9853acafcae3697f69
amira2011 <50692165+amira2011@users.noreply.github.com>
2019-06-24 10:43:23 +0530

    Update movie.rb

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -4,5 +4,17 @@ class Movie < ApplicationRecord
   has_one_attached :image
   scope :search, lambda {|query| where(["title LIKE ?","%#{query}%"])}
 
-  validates :title, :description, :movie_length, :director, :rating, :image, presence: true
+  validates :title, :description, :movie_length, :director, :rating,   presence: true
+  validate :correct_image_type
+
+  private
+
+  def correct_image_type
+    if image.attached? && !image.content_type.in?(%w(image/jpeg image/png))
+      errors.add(:image, 'must be a JPEG or PNG')
+    elsif image.attached? == false
+          errors.add(:image, 'Please select Image ')
+    end
+  end
+
 end
